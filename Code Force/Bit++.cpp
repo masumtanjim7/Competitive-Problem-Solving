98f1c12cb150c3eee8fa3a192c4d1a9a1f5cf7cb
masumtanjim7 <masumbillah19999@gmail.com>
2023-03-26 16:34:38 +0600

    Masum

---
+++ Code Force/Bit++.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    int m, x=0;
+    cin>>m;
+
+    string s;
+    while (m--)
+    {
+        cin>>s;
+        if (s[1]=='+')
+        {
+            ++x;
+        }
+        else
+        {
+            --x;
+        }
+    }
+    cout<<x<<endl;
+return 0;
+}

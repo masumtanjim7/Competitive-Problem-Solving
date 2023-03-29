fed82243944516c381bf710ee40f357321bd25c4
masumtanjim7 <masumbillah19999@gmail.com>
2023-03-29 23:11:20 +0600

    Masum

--- LeetCode/Merge Sorted Array.cpp
+++ LeetCode/Merge Sorted Array.cpp
@@ -9,10 +9,11 @@ public:
         int max = m+n-1;
         int i = m-1;
         int j = n-1;
-        while (i>=0&&j>=0){
+        while (i>=0&&j>=0)
+            {
             if (nums2[j]>nums1[i])nums1[max--]=nums2[j--];
             else nums1[max--]=nums1[i--];
-        }
+            }
         while (j >= 0)nums1[max--] = nums2[j--];
     }
 };
